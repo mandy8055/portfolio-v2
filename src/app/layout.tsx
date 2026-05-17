546550ef7c9b3e8dcaa802103b671481c44d02fe
Manuj Sankrit <msankrit8055@gmail.com>
2026-05-17 17:34:53 +0530

    docs: 📝 add tenth blog on FE architecture

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -88,7 +88,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning data-scroll-behavior='smooth'>
       <head>
         {/* Favicons - you can customize these */}
         <link rel='icon' href='/favicon.ico' sizes='any' />
